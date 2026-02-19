597788c982f09caf5a512e82357d380fbd3b89b8
Davide Marchegiani <davide.marchegiani@anu.edu.au>
2026-02-19 14:58:51 +1100

    Updated recipes and release-package.yaml after migration to GitHub

--- packages/shumlib/recipe/build.sh
+++ packages/shumlib/recipe/build.sh
@@ -23,9 +23,3 @@ make -f $makefile clean-temp
 rm -r ${PREFIX}/tests
 rm ${PREFIX}/include/{fruit.mod,fruit_util.mod}
 rm ${PREFIX}/lib/{libfruit.a,libfruit.so}
-
-# ls $PWD/out/*
-# mkdir -p $PREFIX/{include,lib}
-# cp -v $PWD/out/include/?_shum_*.{h,mod} $PREFIX/include
-# cp -v $PWD/out/include/shumlib_version.h $PREFIX/include
-# cp -v $PWD/out/lib/libshum_*.{a,so} $PREFIX/lib
